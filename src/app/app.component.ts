c7a186aeb4560de512b7045bbb09b214dda0432b
KonectaPe <dtjmestas@gmail.com>
2024-09-29 19:25:25 -0500

    subiendo a netlify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,13 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'galo';
+  listItems: Array<{ id: number; title: string; link: string }> = [
+    { id: 1, title: 'INICIO', link: '#inicio' },
+    { id: 2, title: 'MIS PODERES', link: '#poderes' },
+    { id: 3, title: 'PROYECTOS', link: '#proyectos' },
+    { id: 4, title: 'CONTACTO', link: '#contacto' },
+  ];
 }
